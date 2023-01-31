b6b7a47cc2374eb6ad992c522693f33db3c511aa
LF-Ghost <2771310466@qq.com>
2023-01-31 16:54:52 +0800

    Site updated: 2023-01-31 16:54:51

---
+++ js/search/api.js
@@ -0,0 +1,20 @@
+// 必应每日壁纸
+let bingDayBg = screen.width <= 768 ? "url(https://bing.img.run/m.php)" : "url(https://bing.img.run/1920x1080.php)";
+// 必应历史壁纸
+let bingHistoryBg = screen.width <= 768 ? "url(https://bing.img.run/rand_m.php)" : "url(https://bing.img.run/rand.php)";
+// EEE.DOG动漫壁纸
+let EEEDog = "url(https://api.yimian.xyz/img?type=moe&size=1920x1080)";
+// seovx随机美图
+let seovx = "url(https://cdn.seovx.com/?mom=302)";
+// picsum随机壁纸
+let picsum = "url(https://picsum.photos/1920/1080.webp)";
+// 小歪二次元壁纸
+let waiDongman = "url(https://api.ixiaowai.cn/api/api.php)";
+//  小歪高清壁纸
+let waiBizhi = "url(https://api.ixiaowai.cn/gqapi/gqapi.php)";
+// 博天随机壁纸
+let btstu = "url(http://api.btstu.cn/sjbz/?lx=suiji)";
+// tuapi动漫壁纸
+let tuapi = "url(https://tuapi.eees.cc/api.php?category=dongman)";
+// unsplash随机壁纸 https://source.unsplash.com/random/1920x1080/daily (weekly)
+let unsplash = "url(https://source.unsplash.com/random/1920x1080/)";
